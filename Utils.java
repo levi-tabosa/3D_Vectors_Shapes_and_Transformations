94e17b055d50ee46a15fddf5493f761c8b9ac2c5
levi-tabosa <146300799+levi-tabosa@users.noreply.github.com>
2024-07-03 22:30:30 -0300

    Update Utils.java

--- Utils.java
+++ Utils.java
@@ -1,77 +1,72 @@
 public class Utils {
    public static final TransformFunction projXY = (u, f) -> new V3(
-      u.coord[0], u.coord[1], u.coord[2] * f
+      u.x, u.y, u.z * f[0]
    );
    public static final TransformFunction projXZ = (u, f) -> new V3(
-      u.coord[0], u.coord[1] * f, u.coord[2]
+      u.x, u.y * f[0], u.z
    );
    public static final TransformFunction projYZ = (u, f) -> new V3(
-      u.coord[0] * f, u.coord[1], u.coord[2]
+      u.x * f[0], u.y, u.z
    );
    public static final TransformFunction refX = (u, f) -> new V3(
-      u.coord[0] * f, u.coord[1], u.coord[2]
+      u.x * f[0], u.y, u.z
    );
    public static final TransformFunction refY = (u, f) -> new V3(
-      u.coord[0], u.coord[1] * f, u.coord[2]
+      u.x, u.y * f[0], u.z
    );
    public static final TransformFunction refZ = (u, f) -> new V3(
-      u.coord[0], u.coord[1], u.coord[2] * f
+      u.x, u.y, u.z * f[0]
    );
    public static final TransformFunction rotX = (u, a) -> new V3(
-      u.coord[0],
-      u.coord[1] * Math.cos(a) + u.coord[2] * Math.sin(a),
-      u.coord[2] * Math.cos(a) - u.coord[1] * Math.sin(a)
+      u.x,
+      u.y * Math.cos(a[0]) + u.z * Math.sin(a[0]),
+      u.z * Math.cos(a[0]) - u.y * Math.sin(a[0])
    );
    public static final TransformFunction rotY = (u, a) -> new V3(
-      u.coord[0] * Math.cos(a) - u.coord[2] * Math.sin(a),
-      u.coord[1],
-      u.coord[2] * Math.cos(a) + u.coord[0] * Math.sin(a)
+      u.x * Math.cos(a[0]) - u.z * Math.sin(a[0]),
+      u.y,
+      u.z * Math.cos(a[0]) + u.x * Math.sin(a[0])
    );
    public static final TransformFunction rotZ = (u, a) -> new V3(
-      u.coord[0] * Math.cos(a) + u.coord[1] * Math.sin(a),
-      u.coord[1] * Math.cos(a) - u.coord[0] * Math.sin(a),
-      u.coord[2]
+      u.x * Math.cos(a[0]) + u.y * Math.sin(a[0]),
+      u.y * Math.cos(a[0]) - u.x * Math.sin(a[0]),
+      u.z
    );
    public static final TransformFunction scale = (u, f) -> new V3(
-      u.coord[0] * f,
-      u.coord[1] * f,
-      u.coord[2] * f
+      u.x * f[0],
+      u.y * f[0],
+      u.z * f[0]
    );
-   public static final TranslateFunction translate = (u, p) -> new V3(
-      u.coord[0] + p.coord[0], 
-      u.coord[1] + p.coord[1], 
-      u.coord[2] + p.coord[2]
+   public static final TransformFunction translate = (u, d) -> new V3(
+      u.x + d[0], 
+      u.y + d[1], 
+      u.z + d[2]
    );
-   public static final ShearFunction shearOnX = (u, s, t) -> new V3(
-      u.coord[0], u.coord[1] + u.coord[0] * s, u.coord[2] + u.coord[0] * t
+   public static final TransformFunction shearOnX = (u, s) -> new V3(
+      u.x, u.y + u.x * s[0], u.z + u.x * s[1]
    );
-   public static final ShearFunction shearOnY = (u, s, t) -> new V3(
-      u.coord[0] + u.coord[1] * s, u.coord[1], u.coord[2] + u.coord[1] * t
+   public static final TransformFunction shearOnY = (u, s) -> new V3(
+      u.x + u.y * s[0], u.y, u.z + u.y * s[1]
    );
-   public static final ShearFunction shearOnZ = (u, s, t) -> new V3(
-      u.coord[0] + u.coord[2] * s, u.coord[1] + u.coord[2] * t, u.coord[2]
+   public static final TransformFunction shearOnZ = (u, s) -> new V3(
+      u.x + u.z * s[0], u.y + u.z * s[1], u.z
    );
 }
 interface TransformFunction {
-   V3 apply(V3 u, double f);
+   V3 apply(V3 u, double ... vals);
 }
-interface TranslateFunction {
-   V3 apply(V3 u, V3 p);
-}
-interface ShearFunction {
-   V3 apply(V3 u, double t, double s);
-}
+
 class V3 {
-   double[] coord = new double[3];
+   double x,y,z;
 
    V3(double x, double y, double z) {
-      coord[0] = x;
-      coord[1] = y;
-      coord[2] = z;
+      this.x = x;
+      this.y = y;
+      this.z = z;
    }
 
    @Override
    public String toString() {
-      return String.format("%.2f",coord[0]) + " " + String.format("%.2f",coord[1]) + " " + String.format("%.2f",coord[2]);
+      return String.format("%.2f",x) + "/" + String.format("%.2f",y) + "/" + String.format("%.2f",z);
    }
 }
